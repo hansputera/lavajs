1f2aa7493d8c755c35f431c0244bea536f650371
hansputera <47862061+hansputera@users.noreply.github.com>
2020-06-04 20:56:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-let { exec } = require('child_process');
+let child = require('child_process');
+let os = require('os');
+
+if (!child) throw new Error('Install child_process dependency dulu di package.json');
+
+if (os.type !== 'Linux') throw new Error('Operasi sistem harus linux.');
+
+let exec = child.exec;
 
 // Validasi : Lavalink file
 
-let lava = require('./target/Lavalink-1.0.jar');
+let lava = require('./Lavalink.jar');
 if (lava === undefined) {
 console.log('[INFO] Melakukan Download terhadap Lavalink !');
-exec('wget https://github.com/Frederikam/Lavalink/releases/download/3.3.1/Lavalink.jar', (err, stdout, stderr) => {
+exec('curl https://ci.fredboat.com/repository/download/Lavalink_Build/8110:id/Lavalink.jar?guest=1 -o Lavalink.jar', (err, stdout, stderr) => {
 if (err) {
 return console.log('[INFO] lavalink gagal di download!');
 } 
 else console.log('[INFO] lavalink berhasil di download!');
 
-let lava1 = require('./target/Lavalink-1.0.jar');
+let lava1 = require('./Lavalink.jar');
 if (lava1 === undefined) return console.log('[INFO] Sepertinya tidak ditemukan file Lavalink!');
 });
 console.log('[INFO] Memulai untuk menjalankan Lavalink!');
-exec('java -jar target/Lavalink-1.0.jar', (err, stdout, stderr) => {
+exec('java -jar Lavalink.jar', (err, stdout, stderr) => {
 if (err) {
 return console.log('[INFO] gagal menjalankan lavalink!');
 } else {
@@ -24,7 +31,7 @@ console.log(stdout);
 }
 })
 } else {
-exec('java -jar target/Lavalink-1.0.jar', (err, stdout, stderr) => {
+exec('java -jar Lavalink.jar', (err, stdout, stderr) => {
 if (err) {
 return console.log('[INFO] gagal menjalankan Lavalink! ');
 } else {
